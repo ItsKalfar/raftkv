87853620e4e8b15ce531aa16b84996d85dfee272
Itskalfar <gundsagar02@gmail.com>
2026-08-12 23:21:43 +0530

    working on basic WAL

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+// Build order - WAL -> Election -> Replication -> State machine
+
+mod wal;
+
+use wal::Wal;
+
 fn main() {
-    // Build order - WAL -> Election -> Replication -> State machine
+    let mut wal = Wal::new("app.log").unwrap();
+
+    wal.write_log("helloo").expect("Something went wrong");
+    wal.write_log("its").expect("Something went wrong");
+    wal.write_log("me").expect("Something went wrong");
+    wal.write_log("kalfar").expect("Something went wrong");
+
     println!("Hello, world!");
 }
--- src/wal.rs
+++ src/wal.rs
@@ -1,1 +1,24 @@
+use std::fs::{File, OpenOptions};
+use std::io::Write;
 
+pub struct Wal {
+    file: File,
+}
+
+impl Wal {
+    pub fn new(path: &str) -> std::io::Result<Self> {
+        let file = OpenOptions::new().append(true).create(true).open(path)?;
+
+        Ok(Wal { file: file })
+    }
+
+    pub fn write_log(&mut self, message: &str) -> std::io::Result<()> {
+        self.file
+            .write_all(message.as_bytes())
+            .expect("Something went wrong");
+
+        self.file.sync_data()?;
+
+        Ok(())
+    }
+}
